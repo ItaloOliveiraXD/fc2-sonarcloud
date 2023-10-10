ba8402f725743c1abe44affaf9981413c93be967
Italo Oliveira <italooliveira-xd@hotmail.com>
2023-10-09 22:37:56 -0300

    feat: projeto para aprender sonarcloud

---
+++ go.mod
@@ -0,0 +1,3 @@
+module test/sum
+
+go 1.21.1
---
+++ sum.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(2, 2))
+	fmt.Println(sub(2, 3))
+	fmt.Println(mult(2, 3))
+	fmt.Println(multSum(2, 3))
+}
+
+func sum(a int, b int) int {
+	num1 := a
+	num2 := b
+
+	return num1 + num2
+}
+
+func sub(a int, b int) int {
+	num1 := a
+	num2 := b
+
+	return num1 - num2
+}
+
+func mult(a int, b int) int {
+	num1 := a
+	num2 := b
+
+	return num1 * num2
+}
+
+func multSum(a int, b int) int {
+	num1 := a
+	num2 := b
+	return num1*num2 + (a + b)
+}
